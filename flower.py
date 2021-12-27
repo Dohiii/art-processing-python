35e6bc417d65d4ea3bd126d2d05d18c5db473913
Dohiii <77291884+Dohiii@users.noreply.github.com>
2021-12-27 08:20:57 +0100

    Update flower.py

--- flower.py
+++ flower.py
@@ -1,21 +1,27 @@
+s = second()
+
 TOTAL_DEGREES = 360
+radius = 360
+
 
 def setup():
-    global displayWidth, displayHeight
-    size(displayWidth/2, displayHeight/2)
-    noFill()
-    stroke(255, 25)
+    global displayWidth, displayHeight, radius
+    size(displayWidth, displayHeight )
     background(0)
+    noFill()
+    stroke(0,191,255, 25)
+   
+    radius = height / 2
 
 
 def draw():
+    translate(frameCount, 0)
     # background(0)
-    radius = height / 2
-    center_x = width / 2
+    
+    center_x = 400
     center_y = height / 2
-    
-    global TOTAL_DEGREES
-    
+    global TOTAL_DEGREES, radius, start, s
+
     beginShape()
     for i in range(TOTAL_DEGREES):
         noiseFactor = noise(i * 0.02, float(frameCount) / 150)
@@ -23,3 +29,12 @@ def draw():
         y = center_y + radius * sin(radians(i))* noiseFactor
         curveVertex(x, y)
     endShape(CLOSE)
+
+    radius -= 1
+    
+    if radius == 0:
+        stroke(135,206,250, 25)
+    
+    if radius == -600:
+        noLoop()
+    
